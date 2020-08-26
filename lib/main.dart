19b06d1d2f424c73e81110ba74d8b6021343063c
daiguilin <1535979269@qq.com>
2020-08-26 16:15:50 +0800

    stateFulWidget生命周期讲解

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,13 @@ class MyApp extends StatelessWidget {
 }
 
 class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget() {
+    print("执行了MyStatefulWidget的构造方法");
+  }
+
   @override
   State<StatefulWidget> createState() {
+    print("执行了MyStatefulWidget的createState方法");
     return MyState();
   }
 }
@@ -26,13 +31,47 @@ class MyStatefulWidget extends StatefulWidget {
 class MyState extends State<MyStatefulWidget> {
   int num = 0;
 
+  MyState() {
+    print("执行MyState的构造方法");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    print("执行MyState的init方法");
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    print("执行MyState的didChangeDependencies方法");
+  }
+
+  @override
+  void didUpdateWidget(MyStatefulWidget oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+    print("执行MyState的didUpdateWidget方法");
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print("执行MyState的dispose方法");
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    print("执行执行MyState的build方法");
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Text("计数器值：$num",style: TextStyle(fontSize:24,color: Colors.orange),),
+        Text(
+          "计数器值：$num",
+          style: TextStyle(fontSize: 24, color: Colors.orange),
+        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -46,12 +85,11 @@ class MyState extends State<MyStatefulWidget> {
             RaisedButton(
                 child: Text("-1"),
                 onPressed: () {
-                  if(num!=0){
+                  if (num != 0) {
                     setState(() {
                       num--;
                     });
                   }
-
                 }),
           ],
         )
